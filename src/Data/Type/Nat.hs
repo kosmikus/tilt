d9693dd0973030ddd5e81fd2692490f8d54bc95b
Andres Loeh <mail@andres-loeh.de>
2014-06-02 14:36:43 +0200

    Add quasiquoters.

--- src/Data/Type/Nat.hs
+++ src/Data/Type/Nat.hs
@@ -1,10 +1,12 @@
-{-# LANGUAGE DataKinds, GADTs, TypeOperators, KindSignatures, TypeFamilies, UndecidableInstances, RankNTypes, ScopedTypeVariables, AutoDeriveTypeable, StandaloneDeriving #-}
+{-# LANGUAGE DataKinds, GADTs, TypeOperators, KindSignatures, TypeFamilies, UndecidableInstances, RankNTypes, ScopedTypeVariables, AutoDeriveTypeable, StandaloneDeriving, TemplateHaskell #-}
 module Data.Type.Nat where
 
 import Data.Functor
 import Data.Proxy
 import Data.Typeable
 import Data.Type.Equality
+import Language.Haskell.TH
+import Language.Haskell.TH.Quote
 
 -- | Peano-style natural numbers.
 --
@@ -20,6 +22,35 @@ type N4 = Suc N3
 type N5 = Suc N4
 type N6 = Suc N5
 
+nat :: QuasiQuoter
+nat = QuasiQuoter {
+        quoteExp  = natExp
+      , quotePat  = natPat
+      , quoteType = natType
+      , quoteDec  = error "cannot quote nat in Dec"
+      }
+  where
+    natExp :: String -> Q Exp
+    natExp s = go (read s)
+      where
+        go :: Integer -> Q Exp
+        go 0 = [| Zero |]
+        go n = [| Suc $(go (n - 1)) |]
+
+    natPat :: String -> Q Pat
+    natPat s = go (read s)
+      where
+        go :: Integer -> Q Pat
+        go 0 = [p| Zero |]
+        go n = [p| Suc $(go (n - 1)) |]
+
+    natType :: String -> Q Type
+    natType s = go (read s)
+      where
+        go :: Integer -> Q Type
+        go 0 = [t| Zero |]
+        go n = [t| Suc $(go (n - 1)) |]
+
 data SNat (n :: Nat) where
   SZero :: SNat Zero
   SSuc  :: SNat n -> SNat (Suc n)
@@ -28,6 +59,28 @@ deriving instance Eq (SNat n)
 deriving instance Ord (SNat n)
 deriving instance Show (SNat n)
 deriving instance Typeable SNat
+
+snat :: QuasiQuoter
+snat = QuasiQuoter {
+        quoteExp  = snatExp
+      , quotePat  = snatPat
+      , quoteType = error "cannot quote snat in Type"
+      , quoteDec  = error "cannot quote snat in Dec"
+      }
+  where
+    snatExp :: String -> Q Exp
+    snatExp s = go (read s)
+      where
+        go :: Integer -> Q Exp
+        go 0 = [| SZero |]
+        go n = [| SSuc $(go (n - 1)) |]
+
+    snatPat :: String -> Q Pat
+    snatPat s = go (read s)
+      where
+        go :: Integer -> Q Pat
+        go 0 = [p| SZero |]
+        go n = [p| SSuc $(go (n - 1)) |]
 
 class SNatI (n :: Nat) where
   sNat :: SNat n
